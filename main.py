4af011bc8945f811a36e4957487d11647320031f
Anand M A <9460816+anand-ma@users.noreply.github.com>
2025-01-31 14:26:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from langchain_openai import ChatOpenAI
+# from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain import PromptTemplate
 from langchain import LLMChain
 
